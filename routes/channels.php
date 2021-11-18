9ffb8178e5279051dedebb9ec5d3ed89409e9677
micodev <micoadult2018@gmail.com>
2021-11-18 20:58:43 +0100

    socket

--- routes/channels.php
+++ routes/channels.php
@@ -17,10 +17,7 @@ use Illuminate\Support\Facades\Broadcast;
 //     return (int) $user->id === (int) $id;
 // });
 
-Broadcast::channel('test', function ($user) {
-    error_log("I am here.");
-    return  true;
-});
+
 Broadcast::channel('user_notification.{id}', function ($user, $id) {
     return  $user->id === $id;
 });
@@ -30,13 +27,9 @@ Broadcast::channel('absent_report.{id}', function ($user, $id) {
 Broadcast::channel('general_reports.{school_id}', function ($user, $school_id) {
     error_log($school_id);
     error_log($user->school_id );
-    return true; // $user->school_id === $school_id;
+    return $user->school_id === $school_id;
 });
-Broadcast::channel('private-general_reports.{school_id}', function ($user, $school_id) {
-    error_log($school_id);
-    error_log($user->school_id );
-    return true; // $user->school_id === $school_id;
-});
+
 Broadcast::channel('class_report.{class_id}', function ($user, $class_id) {
     return  $user->class_id === $class_id;
 });
