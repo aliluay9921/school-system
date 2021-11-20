7d68148ed97d3a6e5b601a6755f110f426f2548f
aliluay992 <luaya577@gmail.com>
2021-11-20 19:29:23 +0300

    ali

--- app/Models/FirebaseToken.php
+++ app/Models/FirebaseToken.php
@@ -9,4 +9,10 @@ class FirebaseToken extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+
+    public function user()
+    {
+        return $this->belongsTo(FirebaseToken::class, 'user_id');
+    }
 }
