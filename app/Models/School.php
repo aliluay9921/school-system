9f27f1364a0b926b71a3efc58108d5e2442df275
aliluay992 <luaya577@gmail.com>
2021-11-26 10:27:08 +0300

    ali

--- app/Models/School.php
+++ app/Models/School.php
@@ -10,9 +10,16 @@ class School extends Model
 {
     use HasFactory, Uuids;
     protected $guarded = [];
+    protected $appends = ["CountUser"];
 
     public function users()
     {
         return $this->hasMany(User::class, 'school_id');
+    }
+    // countUser
+    public function getCountUserAttribute()
+    {
+        $school = School::find($this->id);
+        return  $school->users()->where("user_type", 3)->count();
     }
 }
